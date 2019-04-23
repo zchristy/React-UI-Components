99ab7341e9082cb43569d9ead6f5c1c2b2b181da
zchristy <zchristy44@gmail.com>
2019-04-23 13:58:38 -0500

    Completed MVP and Stretch

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Button.css';
+
+class ActionButton extends Component {
+
+  render() {
+    const button = this.props.actButton.map((act, i) => {
+      return <button key={i} className={act.buttonStyle} onClick={this.props.onClick}>{act.text}</button>
+    });
+    return (
+      <div className="action-container">
+        {button}
+      </div>
+    );
+  }
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Button.css';
+
+class NumberButton extends Component {
+
+  render() {
+    const button = this.props.numButton.map((num, i) => {
+      return <button key={i} className={num.buttonStyle} onClick={this.props.onClick}>{num.text}</button>
+    });
+    return (
+      <div className="numbers-container">
+        {button}
+      </div>
+    );
+  }
+}
+
+export default NumberButton;
